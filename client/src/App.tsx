d73c9b294de5923914aa53bc36fc64895109174c
egaotan <chain_dev@163.com>
2023-05-24 19:15:48 +0800

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
 import { Col, Row } from 'antd';
 import Layout from 'antd/es/layout/layout';
 import React from 'react';
+import { WalletSelector } from '@aptos-labs/wallet-adapter-ant-design';
+import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
 
 function App() {
   return (
@@ -12,6 +14,9 @@ function App() {
           </Col>
           <Col span={12} style={{textAlign: "right", paddingRight: "200px"}}>
             <h1> Connect Wallet</h1>
+          </Col>
+          <Col span={12} style={{textAlign: "right", paddingRight: "200px"}}>
+            <WalletSelector />
           </Col>
         </Row>
       </Layout>
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { PetraWallet } from 'petra-plugin-wallet-adapter';
+import { AptosWalletAdapterProvider } from '@aptos-labs/wallet-adapter-react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const wallets = [new PetraWallet()];
+
 root.render(
   <React.StrictMode>
-    <App />
+    <AptosWalletAdapterProvider plugins={wallets} autoConnect={true}>
+      <App />
+    </AptosWalletAdapterProvider>
   </React.StrictMode>
 );
 
