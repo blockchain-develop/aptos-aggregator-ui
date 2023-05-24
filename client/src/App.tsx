9658a8f6499a0a2ee969dd2833a99e6a77920ff6
egaotan <chain_dev@163.com>
2023-05-24 19:47:01 +0800

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,31 @@
-import { Col, Row } from 'antd';
-import Layout from 'antd/es/layout/layout';
-import React from 'react';
+import {Layout, Button, Col, Row } from 'antd';
+import { useState, useEffect } from 'react';
 import { WalletSelector } from '@aptos-labs/wallet-adapter-ant-design';
 import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
+import { Provider, Network } from 'aptos';
+import { useWallet } from '@aptos-labs/wallet-adapter-react';
+
+const provider = new Provider(Network.DEVNET)
+
 
 function App() {
+  const { account } = useWallet();
+  useEffect(() => {
+    fetchList();
+  }, [account?.address]);
+  const [accountHasList, setAccountHasList] = useState<Boolean>(false);
+
+  const fetchList = async() => {
+    if (!account) return [];
+    const moduleAddress = "0xcbddf398841353776903dbab2fdaefc54f181d07e114ae818b1a67af28d1b018";
+    try {
+      const ToolListResource = await provider.getAccountResource(account.address, `${moduleAddress}::todolist::TodoList`);
+      setAccountHasList(true);
+    } catch(e: any) {
+      setAccountHasList(false);
+    }
+  }
+
   return (
     <div>
       <Layout>
@@ -20,6 +41,15 @@ function App() {
           </Col>
         </Row>
       </Layout>
+      {!accountHasList && (
+        <Row gutter={[0, 32]} style={{marginTop: "2rem"}}>
+          <Col span={8} offset={8}>
+            <Button block type="primary" style={{height:"40px", backgroundColor:"#3f67ff"}}>
+              Add new list
+            </Button>
+          </Col>
+        </Row>
+      )}
     </div>
   );
 }
