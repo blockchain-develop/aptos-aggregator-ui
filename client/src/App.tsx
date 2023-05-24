9bd1549de8a19775075780d7509daddcc5d025dc
egaotan <chain_dev@163.com>
2023-05-24 18:18:36 +0800

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,20 @@
+import { Col, Row } from 'antd';
+import Layout from 'antd/es/layout/layout';
 import React from 'react';
 
 function App() {
   return (
     <div>
-      My app goes here
+      <Layout>
+        <Row align="middle">
+          <Col span={10} offset={2}>
+            <h1> Our todolist</h1>
+          </Col>
+          <Col span={12} style={{textAlign: "right", paddingRight: "200px"}}>
+            <h1> Connect Wallet</h1>
+          </Col>
+        </Row>
+      </Layout>
     </div>
   );
 }
